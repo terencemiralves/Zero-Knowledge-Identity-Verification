ca2c1c424d97e9d3cd59b467f7d5ed24caf9e450
terence.miralves <terence.miralves@epita.fr>
2025-06-27 15:19:02 +0200

    fix: added documentation on the proof generator

--- src/proofCreation.js
+++ src/proofCreation.js
@@ -1,6 +1,15 @@
 import { sha256 } from 'js-sha256';
 import * as snarkjs from 'snarkjs';
 
+/*
+ * @description Generates a zk-SNARK proof based on the provided user data.
+ * @param {Object} params - The parameters for proof generation.
+ * @param {string} params.name - The name of the user.
+ * @param {string} params.dob - The date of birth of the user in YYYY-MM-DD format.
+ * @param {string} params.licsense - The license number of the user.
+ * @returns {Promise<Object>} - A promise that resolves to an object containing the proof, public signals, and input data.
+ * @throws {Error} - Throws an error if the proof generation fails.
+ */
 async function generateProof({
     name,
     dob,
@@ -28,6 +37,11 @@ async function generateProof({
     };
 }
 
+/*
+* @description Converts a date string in YYYY-MM-DD format to a Unix timestamp.
+* @param {string} date - The date string to convert.
+* @returns {number} - The Unix timestamp corresponding to the date.
+*/
 function dateToUnix(date) {
     return Math.floor(new Date(date).getTime() / 1000);
 }
